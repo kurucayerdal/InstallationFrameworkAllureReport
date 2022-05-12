c83429a5b631ab7bddf3b642f9a82f4f5429d5e8
Erdal KURUCAY <e.kurucay@Corwin.local>
2022-05-12 10:18:11 +0200

    Allure Report Framework First Commit

---
+++ cypress/integration/Test_Suite_1/testFile.spec.js
@@ -0,0 +1,22 @@
+describe("Example Test Suite", () => {
+    it("Test 1", () => {
+      cy.visit("https://www.google.com");
+      cy.contains("J'accepte").click();
+      cy.get('[name="q"]').type("EVOLUCARE Technologies {enter}");
+    });
+  
+    it("Test 2", () => {
+      cy.visit("https://www.google.com");
+      cy.contains("J'accepte").click();
+      cy.get('[name="q"]').type("EVOLUCARE Technologies {enter}");
+      cy.screenshot();
+    });
+  
+    it("Test 3", () => { //This test case will be failed because of the title of the page
+      cy.visit("https://www.google.com");
+      cy.contains("J'accepte").click();
+      cy.get('[name="q"]').type("EVOLUCARE Technologies {enter}");
+      cy.title().should('eq', 'My Title');
+    });
+  });
+  
